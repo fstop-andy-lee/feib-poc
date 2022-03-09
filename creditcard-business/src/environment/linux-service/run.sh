11aa7aebe6cdad422b713ff3d6e5b718e497dfb2
andy_udp_ip <andy.udp.ip@gmail.com>
2022-03-09 15:39:05 +0800

    a

--- creditcard-business/src/environment/linux-service/run.sh
+++ creditcard-business/src/environment/linux-service/run.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
 #cd /opt/creditcard-business
-#java \
+/c/java/jdk-15.0.1/bin/java \
+  -cp conf:lib/* \
+  -Dspring.application.admin.enabled=false \
+  -Dfile.encoding=UTF8 \
+  -Djava.awt.headless=true \
+	-Djava.security.egd=file:/dev/urandom \
+  -Dlogger.file=conf/logback.xml \
+	-Dloader.main=com.feib.business.api.FeibCreditCardBusinessServiceApplication \
+  -Dspring.profiles.active=dev \
+  -Xms256m \
+  -Xmx1024m \
+  -XX:+HeapDumpOnOutOfMemoryError \
+  -XX:HeapDumpPath=/tmp \
+	org.springframework.boot.loader.PropertiesLauncher
+
+#/c/java/jdk-15.0.1/bin/java \
 #    -cp "conf:lib/*" \
 #    -Dspring.application.admin.enabled=false \
 #    -Dfile.encoding=UTF8 \
 #    -Djava.awt.headless=true \
-#	-Djava.security.egd=file:/dev/urandom \
+#	  -Djava.security.egd=file:/dev/urandom \
 #    -Dlogger.file=conf/logback.xml \
-#	-Dloader.main=com.feib.business.api.FeibCreditCardBusinessServiceApplication \
 #    -Dspring.profiles.active=dev \
 #    -Xms256m \
 #    -Xmx1024m \
 #    -XX:+HeapDumpOnOutOfMemoryError \
 #    -XX:HeapDumpPath=/tmp \
-#	org.springframework.boot.loader.PropertiesLauncher
-
-#java -jar ./lib/creditcard-business-0.0.1.jar --spring.profiles.active=dev
-/c/java/jdk-15.0.1/bin/java -jar ./lib/creditcard-business-0.0.1.jar --spring.profiles.active=dev
+#	com.feib.business.api.FeibCreditCardBusinessServiceApplication
+  
+#/c/java/jdk-15.0.1/bin/java -jar ./lib/creditcard-business-0.0.1.jar --spring.profiles.active=dev
