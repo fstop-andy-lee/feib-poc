78657558bdc33d8684dfac2949be4749c87ddaf6
andy_udp_ip <andy.udp.ip@gmail.com>
2022-03-13 23:18:29 +0800

    a

--- creditcard-integration/src/environment/linux-service/elastic-run.bat
+++ creditcard-integration/src/environment/linux-service/elastic-run.bat
@@ -1,7 +1,7 @@
 
 C:\java\jdk-15.0.1\bin\java ^
     -javaagent:lib/elastic-apm-agent-1.29.0.jar ^
-    -Delastic.apm.service_name=creditcard-business ^
+    -Delastic.apm.service_name=creditcard-integration ^
     -Delastic.apm.server_urls=http://127.0.0.1:8200 ^
     -Delastic.apm.secret_token= ^
     -Delastic.apm.environment=production ^
@@ -11,7 +11,7 @@ C:\java\jdk-15.0.1\bin\java ^
     -Dfile.encoding=UTF8 ^
     -Djava.awt.headless=true ^
     -Dlogger.file=conf/logback.xml ^
-    -Dloader.main=com.feib.business.api.FeibCreditCardBusinessServiceApplication ^
+    -Dloader.main=com.feib.integration.api.FeibCreditCardIntegrationServiceApplication ^
     -Dspring.profiles.active=dev ^
     -Xms256m ^
     -Xmx1024m ^
